dba84028f38cccf665b66e8437eaf6ae90803171
Aditya Dhodade <justtrial892@gmail.com>
2022-04-12 23:30:12 +0530

    converting to remote mysql

--- configuration.php
+++ configuration.php
@@ -1,6 +1,11 @@
 <?php
 
-$con= new mysqli("localhost","user","abcd","search_db");
+// for local environment
+// $con= new mysqli("localhost","user","abcd","search_db");
+
+// for remote mysql
+
+$con= new mysqli("remotemysql.com","OUBM1vrawU","rgyrH6fw2u","OUBM1vrawU");
 
 // for checking connection
 if (mysqli_connect_errno())
